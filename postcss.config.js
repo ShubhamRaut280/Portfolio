02b7ff33c075c936764fedd9754e92ec989b4956
Shubham Raut <rautshubham280@gmail.com>
2026-01-29 00:05:34 +0530

    tailwind and font updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,21 @@
 export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+                display: ['Space Grotesk', 'sans-serif'],
+            },
+            transitionDuration: {
+                '1500': '1500ms',
+                '800': '800ms',
+            }
+        },
+    },
+    plugins: {
+        '@tailwindcss/postcss': {},
+    },
 }
